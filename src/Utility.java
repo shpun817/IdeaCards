d624a2559be3c8a287cf0c005bd02578e3707aea
shpun817 <shpun@connect.ust.hk>
2020-06-23 19:26:14 +0800

    File input done

--- src/Utility.java
+++ src/Utility.java
@@ -1,5 +1,15 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Utility {
 
@@ -15,15 +25,35 @@ public class Utility {
         return null;
     }
 
-    static HashMap<String, CardPile> loadPiles() {
+    static ArrayList<Card> loadCardsFromFile() {
         ArrayList<Card> cards = new ArrayList<>();
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader("C:\\Users\\user\\Desktop\\" + "words.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
 
-        cards.add(new Card("Adjective", "Happy"));
-        cards.add(new Card("Adjective", "Sad"));
-        cards.add(new Card("Noun", "Dog"));
-        cards.add(new Card("Noun", "Cat"));
-        cards.add(new Card("Verb", "Kills"));
-        cards.add(new Card("Verb", "Smiles"));
+            Map<String, List<String>> map = (Map)obj;
+            for (String type : map.keySet()) {
+                List<String> words = map.get(type);
+                for (String word : words)
+                    cards.add(new Card(type, word));
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return cards;
+    }
+
+    static HashMap<String, CardPile> loadPiles() {
+        ArrayList<Card> cards = new Utility().loadCardsFromFile();
 
         HashMap<String, ArrayList<Card>> typeToCards = new HashMap<>();
         for (Card card : cards) {
